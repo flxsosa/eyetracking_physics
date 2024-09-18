5cf151c5eda00c77f8e83667d634121936411d4a
Felix Sosa <felixanthonysosa@gmail.com>
2024-09-18 08:13:46 -0400

    Added data paths

--- config.py
+++ config.py
@@ -8,3 +8,13 @@ EXIT_KEY = 'escape'
 # Monitor setup
 MONITOR_WIDTH = 53.0
 MONITOR_DISTANCE = 70.0
+
+# Global paths
+# Where all data is stored
+DATA_STORE = 'data'
+# Where the stimuli are stored
+STIMULI_STORE = 'data/stimuli'
+# Where the empirical pilot data is stored
+PILOT_STORE = 'data/pilots'
+# Where the stimuli for the introduction trials are stored
+INTRO_TRIAL_STIM_STORE = 'data/introduction'
