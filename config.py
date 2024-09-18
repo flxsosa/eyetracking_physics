d2fd4d31b40a7ae5bf85fbc8af6ead2e97b9d271
Felix Sosa <felixanthonysosa@gmail.com>
2024-09-18 07:22:32 -0400

    Config for global variables.

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
+"""Config file for eye tracking experiment."""
+
 VERSION = 'e3'
+VIDEO_TRIAL_RESPONSE_KEYS = ['f','j']
+CONTINUE_KEY = 'space'
+EXIT_KEY = 'escape'
+
+# Monitor setup
+MONITOR_WIDTH = 53.0
+MONITOR_DISTANCE = 70.0
